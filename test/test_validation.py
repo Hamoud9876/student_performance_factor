b56bd1923fe3baa8ab570711678416ea19e3a25e
Hamoud9876 <h.ghaleb95@gmail.com>
2025-11-12 22:59:43 +0000

    added few test for validation

--- test/test_validation.py
+++ test/test_validation.py
@@ -1,8 +1,24 @@
-from src.validation import validation
+from utils.validation import validation
+import pandas as pd
+import pytest
 
+
+@pytest.fixture(scope="function")
+def create_df():
+    return pd.DataFrame({"id": [1, 2, 3], "name": ["Alice", "Bob", "Charlie"]})
+    
 
 class TestIntigration:
-    def test_handle_wrong_input():
-        pass
+    def test_return_new_df(self, create_df):
+        response = validation(create_df)
+
+        assert response is not create_df
+
+
+    def test_handle_wrong_input(self, create_df, caplog):
+        response = validation("hello")
+
+        assert response is None
+        assert "Wrong df input" in caplog.text
 
 
