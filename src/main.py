d84184f0110e0034080bf479f34fe765c9509099
Hamoud9876 <h.ghaleb95@gmail.com>
2025-11-12 23:00:29 +0000

    imported pandas and read the dataset in csv with pandas to be a df

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
-
+import pandas as pd
+from utils.validation import validation
 
 def main():
+    df = pd.read_csv("../StudentPerformanceFactors.csv")
+
+    df_validated = validation(df)
     pass
