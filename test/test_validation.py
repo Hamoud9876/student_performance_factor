302c4508abaee0e604a16da66fe466d5a60e0d06
Hamoud9876 <h.ghaleb95@gmail.com>
2025-11-13 20:30:58 +0000

    added a new test for schema validation

--- test/test_validation.py
+++ test/test_validation.py
@@ -1,11 +1,15 @@
 from utils.validation import validation
 import pandas as pd
+from io import StringIO
 import pytest
 
 
 @pytest.fixture(scope="function")
 def create_df():
-    return pd.DataFrame({"id": [1, 2, 3], "name": ["Alice", "Bob", "Charlie"]})
+    csv_data = """Hours_Studied,Attendance,Parental_Involvement,Access_to_Resources,Extracurricular_Activities,Sleep_Hours,Previous_Scores,Motivation_Level,Internet_Access,Tutoring_Sessions,Family_Income,Teacher_Quality,School_Type,Peer_Influence,Physical_Activity,Learning_Disabilities,Parental_Education_Level,Distance_from_Home,Gender,Exam_Score
+23,84,Low,High,No,7,73,Low,Yes,0,Low,Medium,Public,Positive,3,No,High School,Near,Male,67
+19,64,Low,Medium,No,8,59,Low,Yes,2,Medium,Medium,Public,Negative,4,No,College,Moderate,Female,61"""
+    return pd.read_csv(StringIO(csv_data))
     
 
 class TestIntigration:
@@ -20,5 +24,12 @@ class TestIntigration:
 
         assert response is None
         assert "Wrong df input" in caplog.text
+
+    
+    def test_validate_schema(self, create_df, caplog):
+        validation(create_df)
+
+        assert "Valid rows: 2" in caplog.text
+        assert "Invalid rows: 0" in caplog.text
 
 
