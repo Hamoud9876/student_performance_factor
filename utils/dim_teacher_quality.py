9d430b96c5695996c1c6e56078ce5a17a6905a0d
Hamoud9876 <h.ghaleb95@gmail.com>
2025-11-14 23:55:02 +0000

    created the logic to transform the df into dim_teacher_quality

---
+++ utils/dim_teacher_quality.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import logging
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="""%(asctime)s [%(levelname)s] %(name)s 
+    (%(filename)s:%(funcName)s:%(lineno)d): %(message)s""",
+    force=True,
+)
+logger = logging.getLogger(__name__)
+
+def dim_teacher_quality(file_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    transform the teacher quality column from in the dataset to
+    match its expected destination
+    Args:
+        file_df: contaning the content of the dataset
+    
+    Returns:
+        a datafram contaning the content and stucture of
+        dim_teacher_quality table
+    """
+    #checking that the input recieved us as expected
+    if not isinstance(file_df, pd.DataFrame):
+        logging.error("Wrong df input")
+        return None
+    
+    #copying the data frame to save the original
+    copy_df = file_df.copy()
+
+    #creating the df that matches the dimension table
+    tq_df = pd.DataFrame()
+
+    try:
+        #taking only unique values
+        tq_df["teacher_quality"] = copy_df["Teacher_Quality"].unique()
+    except Exception as e:
+        logging.error("gender column was not found")
+        return None
+
+
+    return tq_df
