e99c523a1f0668b0cd4aaded0d45dfe489346ea2
Hamoud9876 <h.ghaleb95@gmail.com>
2026-01-30 16:49:57 +0000

    updated the Test class name

--- test/test_dim_teacher_quality.py
+++ test/test_dim_teacher_quality.py
@@ -12,7 +12,7 @@ def create_df():
     return pd.read_csv(StringIO(csv_data))
 
 
-class TestIntigration:
+class TestUnit:
     def test_handle_wrong_input(self, create_df, caplog):
         response = dim_teacher_quality("hello")
 
