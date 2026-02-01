58a3e4b94d44e8081ac1ba6c5e1f6fdf47ed6312
Hamoud9876 <h.ghaleb95@gmail.com>
2026-02-01 22:30:56 +0000

    tests for dim_access_to_reasources

---
+++ test/test_dim_access_to_reasources.py
@@ -0,0 +1,57 @@
+from utils.dim_access_to_resources import dim_access_to_resources
+import pytest
+import pandas as pd
+from io import StringIO
+
+@pytest.fixture(scope="function")
+def create_df():
+    csv_data = """Hours_Studied,Attendance,Parental_Involvement,Access_to_Resources,Extracurricular_Activities,Sleep_Hours,Previous_Scores,Motivation_Level,Internet_Access,Tutoring_Sessions,Family_Income,Teacher_Quality,School_Type,Peer_Influence,Physical_Activity,Learning_Disabilities,Parental_Education_Level,Distance_from_Home,Gender,Exam_Score
+23,84,Low,High,No,7,73,Low,Yes,0,Low,Medium,Public,Positive,3,No,High School,Near,Male,67
+19,64,Medium,Medium,No,8,59,Low,No,2,Medium,Medium,Private,Negative,4,No,College,Moderate,Female,61"""
+    return pd.read_csv(StringIO(csv_data))
+
+
+class TestUnit:
+    def test_handle_wrong_input(self, create_df, caplog):
+        response = dim_access_to_resources("hello")
+
+        assert response is None
+        assert "Wrong df input" in caplog.text
+
+
+    def test_return_new_df(self, create_df):
+        response = dim_access_to_resources(create_df)
+
+        assert response is not create_df
+    
+    def test_logs_error(self, create_df,caplog):
+        df = pd.DataFrame({"SomeColumn": [1, 2, 3]})
+        response = dim_access_to_resources(df)
+
+        assert response is None
+        assert "error while transforming unique access to resources" in caplog.text
+
+        df = pd.DataFrame({})
+        response = dim_access_to_resources(df)
+
+        assert response is None
+        assert "error while transforming unique access to resources" in caplog.text
+
+
+    def test_return_unique_values(self, create_df):
+        response = dim_access_to_resources(create_df)
+
+        assert response.shape[0] == 2
+
+
+    def test_return_correct_values(self,create_df ):
+        response = dim_access_to_resources(create_df)
+
+        assert set(response["access_to_resources"]).issubset({"High", "Medium", "Low"})
+
+
+    def test_output_column_name(self, create_df):
+        result = dim_access_to_resources(create_df)
+
+        assert list(result.columns) == ["access_to_resources"]
+
