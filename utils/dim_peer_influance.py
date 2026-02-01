f36b279d12a548281bab84a56b11e3092146b631
Hamoud9876 <h.ghaleb95@gmail.com>
2026-02-01 22:57:51 +0000

    created the logic to tranform dim_peer_influance

---
+++ utils/dim_peer_influance.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import logging
+from utils.setup_logging import setup_logging
+
+
+logger = logging.getLogger(__name__)
+
+setup_logging()
+
+
+def dim_peer_influance(file_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    transform the peer influance column from in the dataset to
+    match its expected destination
+    Args:
+        file_df: contaning the content of the dataset
+    
+    Returns:
+        a datafram contaning the content and stucture of
+        dim_peer_influance table
+    """
+    #checking that the input recieved us as expected
+    if not isinstance(file_df, pd.DataFrame):
+        logging.error("Wrong df input")
+        return None
+    
+    #copying the data frame to save the original
+    copy_df = file_df.copy()
+
+    #creating the df that matches the dimension table
+    pi_df = pd.DataFrame()
+
+    try:
+        #taking only unique values
+        pi_df["peer_influance"] = copy_df["Peer_Influence"].unique()
+    except Exception as e:
+        logging.error("error while transforming unique peer influance")
+        return None
+
+
+    return pi_df
+
